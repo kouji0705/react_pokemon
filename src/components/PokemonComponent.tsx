dd8af01aa83f5763702a2d691c2aa476b236a744
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-02-04 15:34:24 +0900

    wip:

--- src/components/PokemonComponent.tsx
+++ src/components/PokemonComponent.tsx
@@ -3,11 +3,18 @@ import { usePokemon, usePokemons } from '../api/pokemon/api';
 import { isNullish } from '../common/common';
 
 const PokemonComponent: React.FC = () => {
-  const { data, error, isLoading } = usePokemon('pikachu');
   const { listQuery, detailsQueries } = usePokemons(20, 0);
 
+  if (isNullish(detailsQueries)) return <div>Loading...</div>;
+  console.log('listQuery', listQuery);
+  console.log('HIT8 detailsQueries', detailsQueries);
+  console.log('HIT9 detailsQueries', detailsQueries[0].data);
   // 一覧データのローディング中
-  if (listQuery.isLoading) {
+  if (
+    listQuery.isLoading ||
+    isNullish(listQuery.data) ||
+    isNullish(detailsQueries)
+  ) {
     return <div>Loading pokemons...</div>;
   }
 
@@ -15,21 +22,29 @@ const PokemonComponent: React.FC = () => {
   if (listQuery.isError) {
     return <div>Error: {listQuery.error.message}</div>;
   }
-  if (isLoading || isNullish(data)) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-  console.log('listQuery', listQuery);
-  console.log('HIT8 detailsQueries', detailsQueries);
-  console.log('HIT9 detailsQueries', detailsQueries[0].data);
+
   return (
     <div>
-      <h2>{data.name}</h2>
-      <p>身長: {data.height}</p>
-      <p>体重: {data.weight}</p>
-      {data.sprites.front_default ? (
-        <img src={data.sprites.front_default} alt={data.name} />
-      ) : (
-        <div>画像がありません</div>
-      )}
+      <h2>Pokemon List</h2>
+      <ul>
+        {listQuery.data.results.map((pokemon, index) => (
+          <li key={pokemon.name}>
+            {pokemon.name}
+            {/* 詳細データのローディング状態やエラーをハンドル */}
+            {detailsQueries[index]?.isLoading ? (
+              <span> Loading details...</span>
+            ) : detailsQueries[index]?.isError ? (
+              <span> Error loading details</span>
+            ) : (
+              <div>
+                {/* ここに詳細情報を表示 */}
+                ddd
+                {/* <p>Details: {detailsQueries[index] ?? ''}</p> */}
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
