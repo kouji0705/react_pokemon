e3d7206d34ea3df7017cb874a7836e0cb2abf811
koji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-02-04 15:52:22 +0900

    ポケモンの一覧を取得する (#2)
    * wip:
    
    * wip:
    
    * wip:
    
    * wip:
    
    * wip:

--- src/api/pokemon/api.ts
+++ src/api/pokemon/api.ts
@@ -1,8 +1,9 @@
 // import { Pokemon } from '@bgoff1/pokeapi-types';
 
 import { Pokemon } from '@bgoff1/pokeapi-types';
-import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { UseQueryResult, useQueries, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { PokemonListResponse, UsePokemonsResult } from './type';
 
 export const getPokemon = async (name: string): Promise<Pokemon> => {
   const response = await axios.get<Pokemon>(
@@ -17,4 +18,41 @@ export const usePokemon = (name: string): UseQueryResult<Pokemon, Error> => {
     queryKey: ['pokemon', name],
     queryFn: () => getPokemon(name),
   });
+};
+
+export const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<PokemonListResponse> => {
+  const response = await axios.get<PokemonListResponse>(
+    `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`
+  );
+  return response.data;
+};
+
+export async function getPokemonDetails(url: string): Promise<Pokemon> {
+  const { data } = await axios.get(url);
+  return data;
+}
+
+export const usePokemons = (
+  limit: number,
+  offset: number
+): UsePokemonsResult => {
+  const listQuery = useQuery<PokemonListResponse, Error>({
+    queryKey: ['pokemonList', limit, offset],
+    queryFn: () => getPokemons(limit, offset),
+  });
+
+  // useQueriesは常に呼び出されるが、listQuery.dataが存在する場合のみクエリが有効になる
+  const detailsQueries = useQueries({
+    queries:
+      listQuery.data?.results.map((pokemon) => ({
+        queryKey: ['pokemonDetails', pokemon.name],
+        queryFn: () => getPokemonDetails(pokemon.url),
+        enabled: !!listQuery.data, // listQuery.dataが存在する場合のみクエリを有効化
+      })) ?? [],
+  });
+
+  return { listQuery, detailsQueries };
 };
--- src/components/PokemonComponent.tsx
+++ src/components/PokemonComponent.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
-import { usePokemon } from '../api/pokemon/api';
+import { usePokemon, usePokemons } from '../api/pokemon/api';
 import { isNullish } from '../common/common';
 
 const PokemonComponent: React.FC = () => {
-  const { data, error, isLoading } = usePokemon('pikachu');
+  const { listQuery, detailsQueries } = usePokemons(20, 0);
 
-  if (isLoading || isNullish(data)) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isNullish(detailsQueries)) return <div>Loading...</div>;
+  console.log('listQuery', listQuery);
+  console.log('HIT8 detailsQueries', detailsQueries);
+  // 一覧データのローディング中
+  if (
+    listQuery.isLoading ||
+    isNullish(listQuery.data) ||
+    isNullish(detailsQueries)
+  ) {
+    return <div>Loading pokemons...</div>;
+  }
+
+  // 一覧データ取得時のエラー
+  if (listQuery.isError) {
+    return <div>Error: {listQuery.error.message}</div>;
+  }
 
   return (
     <div>
-      <h2>{data.name}</h2>
-      <p>身長: {data.height}</p>
-      <p>体重: {data.weight}</p>
-      {data.sprites.front_default ? (
-        <img src={data.sprites.front_default} alt={data.name} />
-      ) : (
-        <div>画像がありません</div>
-      )}
+      <h2>Pokemon List</h2>
+      <ul>
+        {detailsQueries.map((query) => {
+          if (query.isLoading || isNullish(query.data)) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <li key={query.data?.id}>
+              <div>{query.data?.id}</div>
+              <div>{query.data?.name}</div>
+              {/* <img src={query.data.}></img> */}
+              <div>
+                <img
+                  // src={query.data.sprites.front_default}
+                  src={query.data.sprites.front_default || ''}
+                  alt={query.data.name}
+                />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
