b3a22e2cfb37b2e58ef95aa31be895e9ffdf0cc6
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-02-04 14:32:11 +0900

    wip:

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// QueryClient インスタンスの作成
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
